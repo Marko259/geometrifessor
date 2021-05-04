5d8b98830607dac4077247b1d105e8d384bb2358
Marko259 <animationsisnice@gmail.com>
2021-05-04 23:19:04 +0200

    Repsonsive design til beregner side + ny JSON data

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,16 +6,31 @@ function beregnC() {
     a = document.getElementById('a').value;
     b = document.getElementById('b').value;
     c = Math.sqrt((a * a) + (b * b));
-    document.getElementById("cer").innerHTML = c;
+    document.getElementById("cer").innerHTML = "C = " + c.toFixed(2);
     /* retter punkterne i svg'en så trekanten bliver flyttet*/
-    document.getElementById("trekant").setAttribute("points", "0,0 0," + a * 20 + " " + b * 20 + "," + a * 20);
+    document.getElementById("retvinklettrekant").setAttribute("points", "0,0 0," + a * 20 + " " + b * 20 + "," + a * 20);
+    RetAreal = a * b / 2
+    document.getElementById("Areal").innerHTML = "Areal = " + RetAreal;
 }
 
 function beregnBen() {
-    ab = document.getElementById("a").value;
-    bb = document.getElementById("b").value;
-    cb = Math.abs(ab);
-    TPb = (bb/2);
-    document.getElementById("cerBen").innerHTML = cb;
-    document.getElementById("ligebenettrekant").setAttribute("points" , TPb + ",0 0," + ab * 20 + " " + bb * 20 + "," + ab * 20)
+    ab = document.getElementById("vilkårlig-a").value;
+    bb = document.getElementById("vilkårlig-b").value;
+    hjd = Math.sqrt((ab * ab) - (bb / 2 * bb / 2));
+    Areal = ab * hjd / 2;
+    TPb = (bb / 2);
+    document.getElementById("vilkårlig-cer").innerHTML = "Areal = " + Areal.toFixed(2);
+    /* retter punkterne i svg'en så trekanten bliver flyttet*/
+    document.getElementById("ligebenettrekant").setAttribute("points", TPb * 20 + ",0 0," + ab * 20 + " " + bb * 20 + "," + ab * 20);
+}
+
+function beregnSide() {
+    as = document.getElementById('ligesidet-a').value;
+    cs = Math.abs(as);
+    TPS = (as / 2);
+    aH = as * Math.sqrt(3) / 2;
+    AreLS = aH * as / 2;
+    document.getElementById("ligesidet-cer").innerHTML = "Areal = " + AreLS.toFixed(2);
+    /* retter punkterne i svg'en så trekanten bliver flyttet*/
+    document.getElementById("ligesidettrekant").setAttribute("points", TPS * 20 + ",0 0," + as * 20 + " " + as * 20 + "," + as * 20);
 }
